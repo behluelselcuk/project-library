bba6aa4731051eb9a627fcdfe1abdc43c20efcf8
Behlül Samil Selcuk <157593831+behluelselcuk@users.noreply.github.com>
2025-05-09 11:19:08 +0200

    Erstelle Gerüst mit Inhalt (JS) zur Bucherstellung
    Es wird das ein Array erstellt, welches mit einer
    Funktion modifiziert werden kann.
    In der Funktion wird auf ein Objektkonstruktor
    zugegriffen, um Bücher-Objekte zu erstellen,
    die in das Array eingefügt werden,

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+}
+
+function addBookToLibrary(title, author, pages, read) {
+
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+
+}
+
+addBookToLibrary('Sherlock Holmes', 'Sir Arthur Conan Doyle', 203, true);
+addBookToLibrary('Quran', 'Allah', 603, false);
+addBookToLibrary('Eine kurze Geschichte der Zeit', 'Stephen Hawking', 271, false);
+addBookToLibrary('Denken wie Sherlock', 'Daniel Smith', 181, false);
+
+console.log(myLibrary);
